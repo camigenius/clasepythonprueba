06cc950ff152789da92fb18fa0c59806e74057fc
camilo franco <camilofinanzas@gmail.com>
2023-08-05 14:48:51 -0500

    miprimeraclase

---
+++ Miprimercodigopython.py
@@ -0,0 +1,15 @@
+# Este el primer codigo en python
+
+# nombre = "Camilo"
+# apellido = "Franco"
+# print("Mi nombre es " + nombre + apellido)
+# print(nombre,apellido)
+# print(f"Mi nombre {nombre} y mi apellido es {apellido}")
+# print("*****************************")
+# print("Lenguajes favoritos :\npython")
+# print("*"*30)
+
+nombre_cliente = input("Ingresa tu nombre ")
+print(nombre_cliente)
+print("Bienvenido al supermercado Sr(a)",nombre_cliente.title())
+
